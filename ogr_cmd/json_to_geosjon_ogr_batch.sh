2241f48abd97fc0f1f3515e7cdb2b5c7f5baba65
alxbishop <alexbishop11@gmail.com>
2024-02-12 16:53:01 -0500

    first commit

---
+++ ogr_cmd/json_to_geosjon_ogr_batch.sh
@@ -0,0 +1,24 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2638
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 .AppleSystemUIFontMonospaced-Regular;\f1\fswiss\fcharset0 Helvetica;\f2\fnil\fcharset0 Menlo-Regular;
+}
+{\colortbl;\red255\green255\blue255;\red0\green0\blue0;\red0\green0\blue0;}
+{\*\expandedcolortbl;;\cssrgb\c0\c0\c0;\csgray\c0;}
+\margl1440\margr1440\vieww13940\viewh7400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 #!/bin/bash\
+\pard\pardeftab720\partightenfactor0
+\cf0 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 for filename in ./../*.shp; do\
+	echo filename
+\f1 \kerning1\expnd0\expndtw0 \outl0\strokewidth0 ; 
+\f0 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 \
+    done\
+done\
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+\cf0 \kerning1\expnd0\expndtw0 \outl0\strokewidth0 \
+\
+
+\f2\fs22 \cf3 \CocoaLigature0 \
+}
